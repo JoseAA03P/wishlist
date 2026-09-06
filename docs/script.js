16a8c7f155359739c4c67900eb8b84346745c067
JoseAA03P <jose.palomaresvz@udlap.mx>
2026-09-05 19:10:47 -0600

    describe your changes here

--- docs/script.js
+++ docs/script.js
@@ -9,66 +9,66 @@ const wishlist = document.getElementById("wishlist");
 const products = [
 
     {
-        name: "Bálsamo Black Honey - Clinique",
-        image: "images/blackhoneylipstick.avif",
-        price: "$499",
-        link: "https://www.clinique.com.mx/product/1605/4772/maquillaje/labiales/balsamo-labial-almost-lipstick?srsltid=AfmBOopGf3mIEtHIYEpiBFwN1mFi12y13Ks1oYuZkvd5efIF-JipIeSx&shade=Black_Honey"
+        name: "Explorer - Montblanc",
+        image: "images/montblanc.jpeg",
+        price: "$531",
+        link: "https://www.mercadolibre.com.mx/perfume-montblanc-explorer-100-ml/p/MLM16141387?pdp_filters=item_id%3AMLM1887180221&matt_tool=17030900&ua=ha5fdsIirZxuQfe91X0mYJX8rk1URj6qdkLpUgXx8A53FuU&offer_type=BEST_PRICE"
     },
 
     {
-        name: "Delineador Black Honey - Clinique",
-        price: "$450",
-        image: "images/blackhoneyliner.avif",
-        link: "https://www.clinique.com.mx/product/1604/4771/maquillaje/delineadores-de-labios/delineador-de-labios-quickliner-for-lips?shade=BLACK_HONEY"
+        name: "Velas aromaticas",
+        price: "$258",
+        image: "images/velas.jpeg",
+        link: "https://a.co/d/0iQFKzA7"
     },
 
     {
-        name: "Show Off Bra - Aerie",
-        price: "$1,199",
-        image: "images/aerieshowoffbra.webp",
-        link: "https://www.ae.com/mx/es/p/aerie/bras/unlined-bras/show-off-embroidery-unlined-bra/2791_5301_125"
+        name: "Calzado senderismo",
+        price: "$599",
+        image: "images/senderismo.jpeg",
+        link: "https://www.decathlon.com.mx/descubre-mas-de-65-deportes/montana/calzado-senderismo"
     },
 
     {
-        name: "Rimel color borgoña - Bailando Juntos (Yuya)",
-        price: "$219",
-        image: "images/yuyarimelborgona.webp",
-        link: "https://yuyatiendaoficial.com/collections/ojos/products/1-mascara-de-pestanas-10-aceites-naturales-borgona"
+        name: "Mochila cool (cuero)",
+        price: "$407",
+        image: "images/mochila.webp",
+        link: "https://a.co/d/08C535a7"
     },
 
     {
-        name: "Gel fijador de cejas - Bailando Juntos (Yuya)",
+        name: "Reloj Casio Vintage Digital Negro Origina",
+        price: "$550",
+        image: "images/casio.webp",
+        link: "https://www.mercadolibre.com.mx/up/MLMU4061577586?matt_tool=17030900&pdp_filters=item_id:MLM2985793727&ua=MzQrfqDCC6ZVwNA9G1Hvd3zGqze9Cklunos55KPHwsVvrQo#origin=share&sid=share&wid=MLM2985793727&action=copy"
+    },
+
+    {
+        name: "Taza cool",
+        price: "$254",
+        image: "images/taza.jpg",
+        link: "https://a.co/d/0djFeQME"
+    },
+
+    {
+        name: "Termo owala",
+        price: "$377",
+        image: "images/owala.webp",
+        link: "https://www.mercadolibre.com.mx/p/MLM26254901?matt_tool=17030900&pdp_filters=item_id:MLM6089507246&ua=v-amche7xLPdY752g-fwOU3-RqJA0tBFw8i8Wuy7o1e9dMo#origin=share&sid=share&wid=MLM6089507246&action=copy"
+    },
+
+    {
+        name: "Playeras para el gimnasio",
+        price: "$298",
+        image: "images/gym.webp",
+        link: "https://www.mercadolibre.com.mx/paquete-de-4-playeras-deportivas-de-licra-de-secado-rapido/up/MLMU3866555661?pdp_filters=item_id%3AMLM5098644280&from=gshop&matt_tool=57911886&matt_word=&matt_source=google&matt_campaign_id=23406600365&matt_ad_group_id=193915119354&matt_match_type=&matt_network=g&matt_device=c&matt_creative=790322144558&matt_keyword=&matt_ad_position=&matt_ad_type=pla&matt_merchant_id=5581263552&matt_product_id=MLMU3866555661&matt_product_partition_id=2493287457326&matt_target_id=aud-2006290659986:pla-2493287457326&cq_src=google_ads&cq_cmp=23406600365&cq_net=g&cq_plt=gp&cq_med=pla&gad_source=1&gad_campaignid=23406600365&gbraid=0AAAAAoTLPrIkigJY2-7fyjZuM_EZ7tADg&gclid=EAIaIQobChMI8KCn2uHYlgMVyitECB3H1ysxEAQYASABEgKpG_D_BwE"
+    },
+
+    {
+        name: "Power Bank",
         price: "$199",
-        image: "images/yuyacelfijadorcejas.webp",
-        link: "https://yuyatiendaoficial.com/collections/ojos/products/gel-fijador-para-cejas"
-    },
-
-    {
-        name: "Pestañas postizas individuales",
-        price: "$179",
-        image: "images/amazonlashes.jpg",
-        link: "https://a.co/d/0imMAUqO"
-    },
-
-    {
-        name: "Serum corporal Niacinamida - Dove",
-        price: "",
-        image: "images/doveserumniacinamida.avif",
-        link: "https://www.dove.com/mx/p/dove-serum-corporal-tonounif-niaci200.html/07506306252837"
-    },
-
-    {
-        name: "Ácido glicólico al 7% (100 ml) - The Ordinary",
-        price: "$215",
-        image: "images/ordinaryacidoglicolico.jpg",
-        link: "https://a.co/d/08cx8JPr"
-    },
-
-    {
-        name: "Tubos de satín para cabello",
-        price: "$199",
-        image: "images/amazonheatlesscurls.jpg",
-        link: "https://a.co/d/0hRcuktM"
+        image: "images/power.webp",
+        link: "https://a.co/d/0hxm98Tn"
     }
 ];
 
